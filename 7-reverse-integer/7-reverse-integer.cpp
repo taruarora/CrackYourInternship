d58619cdffce03cab734554ce1cd852b7cd3dcae
taruarora <75572067+taruarora@users.noreply.github.com>
2022-06-23 23:26:36 +0530

    Time: 3 ms (63.12%), Space: 6 MB (31.61%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+       long int s=0;
+        while(x)
+        {
+            int r=x%10;
+            x=x/10;
+            s=s*10+r;
+            
+        }
+         if(s < -pow(2,31) || s > pow(2,31)-1)
+           return 0;
+        return int(s);
+        
+    }
+};
