3d9bfe219e6e449c48586f762fa8964f6671843b
taruarora <75572067+taruarora@users.noreply.github.com>
2022-08-14 12:20:37 +0530

    Time: 1 ms (79.59%), Space: 42.5 MB (19.26%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        int l=s.length();
+        char[] ch=new char[l];
+        for(int i=0;i<l;i++)
+        {
+            if(s.charAt(i)>='A'&& s.charAt(i)<='Z')ch[i]=(char)((int)s.charAt(i)+32);
+            else ch[i]=s.charAt(i);
+        }
+    
+        return new String(ch);
     }
 }
