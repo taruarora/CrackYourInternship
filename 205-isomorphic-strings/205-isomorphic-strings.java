a0600c17974629207cd29ff2ad521b2df0c65daf
taruarora <75572067+taruarora@users.noreply.github.com>
2022-08-17 01:32:41 +0530

    Time: 9 ms (70.58%), Space: 43.7 MB (15.72%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int sl=s.length();
+        int i=t.length();
+        if(sl!=i)return false;
+        i=0;
+        int m=0;
+        for(int j=0;j<sl;j++)
+        {
+            int n =s.indexOf(s.charAt(i))^ t.indexOf(t.charAt(i));
+            if(n!=0)return false;
+            i++;
+        }
+        return true;
+        
+    } 
+}
