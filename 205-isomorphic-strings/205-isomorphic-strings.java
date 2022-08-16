bd08a986020e5d23f3343a38dafa7ae6e074dd09
taruarora <75572067+taruarora@users.noreply.github.com>
2022-08-17 01:35:12 +0530

    Time: 8 ms (76.97%), Space: 42.2 MB (93.56%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -4,10 +4,10 @@ class Solution {
         int i=t.length();
         if(sl!=i)return false;
         i=0;
-        int m=0;
+        int n=0;
         for(int j=0;j<sl;j++)
         {
-            int n =s.indexOf(s.charAt(i))^ t.indexOf(t.charAt(i));
+            n =s.indexOf(s.charAt(i))^ t.indexOf(t.charAt(i));
             if(n!=0)return false;
             i++;
         }
