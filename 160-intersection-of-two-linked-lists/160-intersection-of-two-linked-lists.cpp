219601f5fb9c63482b9415ff5420b30202570392
taruarora <75572067+taruarora@users.noreply.github.com>
2022-08-24 11:51:15 +0530

    Time: 55 ms (80.71%), Space: 14.6 MB (73.59%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -5,25 +5,53 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
- /////////VIA MAP STORING ADDRESSES
+ /////////VIA LINKED LIST LENGTH
  */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *ptr=headA;
-        map<ListNode*, int> m;
-        while(ptr)
+        ListNode *ptr1=headA;
+        ListNode *ptr2=headB;
+        ListNode *ptr;
+        int p1=0, p2=0;
+        
+        while(ptr1)
         {
-            m[ptr]++;  
-            ptr=ptr->next;
+            p1++;
+            ptr1=ptr1->next;
         }
-        ptr=headB;
-        while(ptr)
+        
+        
+        while(ptr2)
         {
-            if(m.find(ptr)!=m.end()){
-                return ptr;}
-            ptr=ptr->next;
+            p2++;
+            ptr2=ptr2->next;
         }
+        
+       
+        if(p2>p1)
+        {
+           ptr2=headA;
+           ptr1=headB;
+        }
+         else
+        {
+           ptr1=headA;
+           ptr2=headB;
+        }
+        
+        for(int i=0;ptr1 && i<abs(p1-p2) ;i++)
+        {
+            cout<<ptr1->val;
+            ptr1=ptr1->next;
+        }
+        for(int i=0;i<p2;i++)
+        {
+            if(ptr1==ptr2)return ptr1;
+            ptr1=ptr1->next;
+            ptr2=ptr2->next;
+        }
+        
         return NULL;
     }
 };
