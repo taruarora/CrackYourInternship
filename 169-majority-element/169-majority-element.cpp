4f6bc1c735059c6351899e927bef96b43cb938fa
taruarora <75572067+taruarora@users.noreply.github.com>
2022-08-25 20:28:59 +0530

    Time: 46 ms (11.79%), Space: 19.6 MB (59.64%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         map<int,int>m;
+        int n=nums.size();
         for(int i=0;i<nums.size();i++)
         {
            m[nums[i]]++; 
+           if(m[nums[i]]>n/2)return nums[i];
         }
-        pair<int,int> p=make_pair(0,0);
-        map<int,int> ::iterator i;
-        for(i=m.begin();i!=m.end();i++)
-        {
-            if(i->second > p.second)p=make_pair(i->first,i->second);
-        }
-        return p.first;
+        return -1;
+        
     }
 };
