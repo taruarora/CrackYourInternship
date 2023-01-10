3ce0d8140806d07e758641c48b8bfc6846c24f4d
taruarora <75572067+taruarora@users.noreply.github.com>
2023-01-11 02:38:11 +0530

    Time: 5 ms (32.76%), Space: 6.9 MB (93.60%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,20 @@
+#include<cmath>
+class Solution {
+public:
+    void rotate(vector<vector<int>>& arr) {
+        int n=arr.size();
+        int m=arr[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(arr[i][j],arr[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            reverse(arr[i].begin(),arr[i].end());
+        }
+        
+    }
+};
