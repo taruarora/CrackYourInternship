efc4db55da4508e085f3a0c14a3ebdb28fd30be2
taruarora <75572067+taruarora@users.noreply.github.com>
2023-03-02 12:56:33 +0530

    Added solution - LeetHub

--- Power of 2 - GFG/power-of-2.cpp
+++ Power of 2 - GFG/power-of-2.cpp
@@ -20,7 +20,7 @@ class Solution{
         */
         if(n==0)return false;
         if(n==1)return true;
-        while(n!=1)
+        while(n>1)
         {
             if(n%2!=0)return false;
             n=n/2;
