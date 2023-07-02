0d21ba9fa041c479a329322e3d8be41edfbbf0b8
Taru Arora <75572067+taruarora@users.noreply.github.com>
2023-07-03 00:40:40 +0530

    Time: 32 ms (92.86%), Space: 10.8 MB (73.30%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -61,13 +61,14 @@ public:
         int n=str1.length();
         int m=str2.length();
         int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
+        for(int i=0;i<=n;i++)
+         dp[i][0]=0;
+        for(int i=0;i<=m;i++)
+         dp[0][i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
 
-                else if(str1[i-1]==str2[j-1]){
+                 if(str1[i-1]==str2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
 
