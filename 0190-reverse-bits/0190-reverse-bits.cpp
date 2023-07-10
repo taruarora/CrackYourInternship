afa98b1ed574bcd3f10176fdec542bfcbc01da1e
Taru Arora <75572067+taruarora@users.noreply.github.com>
2023-07-11 00:03:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (65.52%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int low,high;
+        unsigned int result=0;
+        for(int i=0;i<32;i++)
+        {
+           int lsb = n&1;
+            int revLsb = lsb<<(31-i);
+            result = result | revLsb;
+            n = n>>1;
+               
+        }
+        return result;
+    }
+};
