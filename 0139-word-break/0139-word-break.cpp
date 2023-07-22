7acd0785f1958fd6a63c3bf6ca758fa76e85800a
Taru Arora <75572067+taruarora@users.noreply.github.com>
2023-07-22 17:52:02 +0530

    Time: 20 ms (42.08%), Space: 14.4 MB (30.27%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int recur(int ind,string A, vector<string> &B,int n, string s,vector<int>& dp)
+    int recur(int ind,string A, vector<string> &B,int n, vector<int>& dp)
     {
-        if(s==A || ind==A.length())return 1;
+        if( ind==A.length())return 1;
         if(ind>A.length())return 0;
         
         if(dp[ind]!=-1)return dp[ind];
-        for(int i=0;i<A.length();i++)
+        for(int i=ind;i<A.length();i++)
         {
-            string str=A.substr(ind,i+1);
+            string str=A.substr(ind,i-ind+1);
             if(find(B.begin(),B.end(),str)!=B.end())
              {
                  //cout<<str<<endl;
-                  if(recur(i+ind+1,A,B,n,s+str,dp)==1)return dp[ind]= 1;
+                  if(recur(i+1,A,B,n,dp)==1)return dp[ind]= 1;
              }
         }
         return dp[ind]= 0;
@@ -20,6 +20,6 @@ public:
     bool wordBreak(string A, vector<string>& B) {
         string s="";
         vector<int> dp(301,-1);
-        return recur(0, A,B,B.size(),s,dp);
+        return recur(0, A,B,B.size(),dp);
     }
 };
